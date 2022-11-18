8ccb88835a043d23c6077bc05f04b3a644e733f8
ezhomelabs <support@haicam.tech>
2022-11-18 23:26:01 +1100

    add bloc models

---
+++ haicam/lib/blocs/models/account.dart
@@ -0,0 +1,7 @@
+import 'package:prac_haicam/blocs/models/camera.dart';
+import 'package:prac_haicam/blocs/models/user.dart';
+class Account {
+  int? id;
+  List<User> users = [];
+  List<Camera> cameras = [];
+}
---
+++ haicam/lib/blocs/models/camera.dart
@@ -0,0 +1,17 @@
+class Camera {
+  int? id;
+  int? addrId;
+  int? cameraType;
+  int? hubId;
+  int? hubType;
+  String? serialNumber;
+  String? caemraName;
+  String? videoSize; // "1920:1080"
+  String? lastImage; // local jpg image path
+  String? hubName;
+  bool? isOnline;
+  DateTime? onlineSince;
+  DateTime? offlineSince;
+  DateTime? dateCreated;
+  DateTime? dateUpdated;
+}
